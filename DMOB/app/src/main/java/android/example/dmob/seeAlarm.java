7b9b4170b0560129791032fe4fc187f7c14a882e
lucas-mdrdn <lucas.mdrdn@eduge.ch>
2022-03-14 17:08:50 +0100

    add xml seeAlarm

---
+++ DMOB/app/src/main/java/android/example/dmob/seeAlarm.java
@@ -0,0 +1,14 @@
+package android.example.dmob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seeAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_alarm);
+    }
+}
