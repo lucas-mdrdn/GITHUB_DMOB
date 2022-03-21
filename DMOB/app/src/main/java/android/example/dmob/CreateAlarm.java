7334b8d411a8ee59134bf031a0dbf3eb178cd15c
lucas-mdrdn <lucas.mdrdn@eduge.ch>
2022-03-21 17:08:50 +0100

    début de l'intégration de l'API Location

--- DMOB/app/src/main/java/android/example/dmob/CreateAlarm.java
+++ DMOB/app/src/main/java/android/example/dmob/CreateAlarm.java
@@ -1,23 +1,90 @@
 package android.example.dmob;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 public class CreateAlarm extends AppCompatActivity {
+    Button LocationButton;
+    View PosX, PosY;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_alarm);
+
+        LocationButton = findViewById(R.id.LocationButton);
+        PosX = findViewById(R.id.PosX);
+        PosY = findViewById(R.id.PosY);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationButton.setOnClickListener(new View.onClickListener() {
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(CreateAlarm.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(CreateAlarm.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        Geocoder geocoder = new Geocoder(CreateAlarm.this, Locale.getDefault());
+                        List<Address> adresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        })
     }
 
     public void launchMainActivity(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
+
 
     class alarm {
         float x;
